c7c49e1b7dc40c765ecfb9cc34ffd1cc8e087d09
Kin Ian Lo <keonlo123@gmail.com>
2019-07-08 15:52:53 +0100

    Rewrite utils.py object-orientedly

--- utils.py
+++ utils.py
@@ -8,11 +8,28 @@ Created on Mon Jul  8 15:33:28 2019
 
 from qutip import basis, tensor
 
-GROUND = 1
-EXCITED = 0
-
-def ion_state(n_internal, n_fock, num_focks):
-    return tensor(basis(2, n_internal), basis(num_focks, n_fock))
-
-# alias
-ionst = ion_state
+class IonStateFactory:
+    """
+    Provide a concise way to generate ion trap states. 
+    """
+    def __init__(self, num_focks):
+        self.num_focks = num_focks
+        
+    def exd(self, n_fock):
+        """
+        Return an excited state in the n_fock Fock state 
+        """
+        self._verify_n_fock(n_fock)
+        return tensor(basis(2, 0), basis(self.num_focks, n_fock))
+    
+    def gnd(self, n_internal, n_fock):
+        """
+        Return a ground state in the n_fock Fock state 
+        """
+        self._verify_n_fock(n_fock)
+        return tensor(basis(2, 1), basis(self.num_focks, n_fock))
+    
+    def _verify_n_fock(self, n_fock):
+        if n_fock >= self.num_focks:
+            raise ValueError('n_fock must be less then num_focks')
+        
