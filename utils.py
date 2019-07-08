4040777e3b130cebdef73a0143fb199a702bd476
Kin Ian Lo <keonlo123@gmail.com>
2019-07-08 15:55:17 +0100

    fix bug

--- utils.py
+++ utils.py
@@ -10,26 +10,25 @@ from qutip import basis, tensor
 
 class IonStateFactory:
     """
-    Provide a concise way to generate ion trap states. 
+    Provide a concise way to generate ion trap states.
     """
     def __init__(self, num_focks):
         self.num_focks = num_focks
-        
+
     def exd(self, n_fock):
         """
-        Return an excited state in the n_fock Fock state 
+        Return an excited state in the n_fock Fock state
         """
         self._verify_n_fock(n_fock)
         return tensor(basis(2, 0), basis(self.num_focks, n_fock))
-    
-    def gnd(self, n_internal, n_fock):
+
+    def gnd(self, n_fock):
         """
-        Return a ground state in the n_fock Fock state 
+        Return a ground state in the n_fock Fock state
         """
         self._verify_n_fock(n_fock)
         return tensor(basis(2, 1), basis(self.num_focks, n_fock))
-    
+
     def _verify_n_fock(self, n_fock):
         if n_fock >= self.num_focks:
             raise ValueError('n_fock must be less then num_focks')
-        
